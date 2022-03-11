ff16efac1d89ead1e91245c072fcf96f59c6261b
Tainá Vilela <tainadantas89@hotmail.com>
2022-03-11 14:14:48 -0300

    alterando o script

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const comeco = document.getElementById('comeco')
 const imagem = document.getElementById('imagem1')
-let botao = document.querySelector('button')
+let botao = document.getElementById('button')
 
-elementoBtn.addEventListener('click', ()=>{
-    if(botao.value == "Click"){
-        imagem.src = './assets/img/img1.jpg'
+botao.addEventListener('click', ()=>{
+    if(botao.value == "click"){
+        imagem.src = './assets/img/img2.jpg'
         botao.value = 'abc'
     }
 
     else if(botao.value == 'abc'){
-        imagem.src = './assets/img/img2.jpg'
+        imagem.src = './assets/img/img3.jpg'
         botao.value= 'bcd'
     }
     else {
         imagem.src = './assets/img/img3.jpg'
-        botao.value = 'Click'
+        botao.value = 'click'
     }
 })
